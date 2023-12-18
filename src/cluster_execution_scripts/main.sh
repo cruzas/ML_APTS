d5656c99159eeaca78f2cbf14a3c6c4f89dcf301
Ken Trotti <ken13889@msn.com>
2023-12-18 16:31:01 +0100

    update

--- src/cluster_execution_scripts/main.sh
+++ src/cluster_execution_scripts/main.sh
@@ -3,12 +3,10 @@
 echo "Running main.sh...."
 
 nr_models_array=(1 2)
-if [ "$do_APTS_W_tests" -eq 1 ]; then
-    echo "Current directory is $PWD"
-    echo "Counter before starting APTS_W tests: $counter"
-    IFS=',' 
-    echo "Trying to calling apts_w.sh..."
-        ./apts_w.sh \
-    --nr_models_array "${nr_models_array[*]}" 
-    sleep 1
-fi
+echo "Current directory is $PWD"
+echo "Counter before starting APTS_W tests: $counter"
+IFS=',' 
+echo "Trying to calling apts_w.sh..."
+    ./apts_w.sh \
+--nr_models_array "${nr_models_array[*]}" 
+sleep 1
