360ccca9e0e902db4268a0a60d520edc3125956b
Samuel Cruz <cruzas@users.noreply.github.com>
2024-03-26 09:29:56 +0100

    update: for daint

--- src/cluster_execution_scripts/apts_w.sh
+++ src/cluster_execution_scripts/apts_w.sh
@@ -13,6 +13,11 @@ while [[ $# -gt 0 ]]; do
             IFS=',' read -ra nr_models_array <<< "$1"
             shift
             ;;
+        --minibatch_sizes)
+            shift
+            IFS=',' read -ra batch_sizes <<< "$1"
+            shift
+            ;;
         *)
             echo "Unknown argument: $1"
             exit 1
@@ -24,19 +29,25 @@ done
 IFS=','  # Set the field separator to comma
 echo "Optimizer name: $optimizer_name"
 echo "Number of Models Array: ${nr_models_array[*]}"
+echo "Batch sizes: ${minibatch_sizes[*]}"
+
 
 for nr_models in "${nr_models_array[@]}"; do
-    # Skip the following code if both momentum and second order equal 1
-    # Arguments to be (finally) passed to the python script
-    ARGS=(
-         --optimizer_name="$optimizer_name"
-         --nr_models="$nr_models"
-    )
-    job_name="./src/cluster_execution_scripts/${optimizer_name}_${nr_models}.job"
-    output_filename="${job_name}.out"
-    error_filename="${job_name}.err"
-    sbatch --nodes=$nr_models --job-name=$job_name --output=$output_filename --error=$error_filename ./src/cluster_execution_scripts/apts_w.job 
-    echo "Submitted APTS_W test with counter $counter"
+    for minibatch_size in "${minibatch_sizes[@]}"; do
+        counter=$((counter+1))
+        # Skip the following code if both momentum and second order equal 1
+        # Arguments to be (finally) passed to the python script
+        ARGS=(
+             --optimizer_name="$optimizer_name"
+             --nr_models="$nr_models"
+             --minibatch_size="$minibatch_size"
+        )
+        job_name="${optimizer_name}_${nr_models}_${batch_size}.job"
+        output_filename="${job_name}.out"
+        error_filename="${job_name}.err"
+        sbatch --nodes=$nr_models --job-name=$job_name --output=$output_filename --error=$error_filename ./src/cluster_execution_scripts/apts_w.job "${ARGS[@]}" 
+        echo "Submitted APTS_W test."
+    done
 done
 
 
--- src/cluster_execution_scripts/main.sh
+++ src/cluster_execution_scripts/main.sh
@@ -3,7 +3,10 @@
 echo "Running main.sh...."
 
 nr_models_array=(15 30 60)
+minibatch_sizes=(10000 60000)
 IFS=',' 
 echo "Trying to calling apts_w.sh..."
     ./apts_w.sh \
---nr_models_array "${nr_models_array[*]}" 
+--nr_models_array "${nr_models_array[*]}"\
+--minibatch_sizes "${minibatch_sizes[*]}"
+ 
