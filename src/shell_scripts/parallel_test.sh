3087e1d4d7df3e46b9780283fcb14ee3e11d4c72
Samuel Adolfo Cruz Alegria <scruzale@daint104.login.cscs.ch>
2024-05-17 10:09:31 +0200

    Removing num workers for DataLoader as it led to problems.

--- src/shell_scripts/parallel_test.sh
+++ src/shell_scripts/parallel_test.sh
@@ -3,8 +3,6 @@
 # Define the arrays for each parameter
 optimizers=("Adam")
 batch_sizes=(200)
-# learning_rates=(0.01 0.02 0.04 0.08 0.16 0.32 0.64 1.0)
-learning_rates=(0.001)
 trials=5
 epochs=50
 nodes=2
@@ -13,19 +11,24 @@ datasets=("cifar10")
 # Iterate over each combination of parameters
 for optimizer in "${optimizers[@]}"
 do
+    # If optimizer is SGD, set learning rate to 0.1. Else, 0.001
+    if [ "$optimizer" == "SGD" ]
+    then
+        learning_rate=0.1
+    else
+        learning_rate=0.001
+    fi
+
     for batch_size in "${batch_sizes[@]}"
-    do
-        for learning_rate in "${learning_rates[@]}"
+    do   
+        for dataset in "${datasets[@]}"
         do
-            for dataset in "${datasets[@]}"
-            do
-                # Make job name depend on the parameters
-                job_name="test_${optimizer}_${batch_size}_${learning_rate}_${dataset}"
-                error_file="${job_name}.err"
-                output_file="${job_name}.out"
-                
-                sbatch --nodes=$nodes --job-name="$job_name" --output="$output_file" --error="$error_file" parallel_test.job "$optimizer" "$batch_size" "$learning_rate" "$trials" "$epochs" "$dataset"
-            done
+            # Make job name depend on the parameters
+            job_name="test_${optimizer}_${batch_size}_${learning_rate}_${dataset}"
+            error_file="${job_name}.err"
+            output_file="${job_name}.out"
+            
+            sbatch --nodes="$nodes" --job-name="$job_name" --output="$output_file" --error="$error_file" parallel_test.job "$optimizer" "$batch_size" "$learning_rate" "$trials" "$epochs" "$dataset"
         done
     done
 done
