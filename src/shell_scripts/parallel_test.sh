adc7870db120267df0aa07285b18bd8d9f89e41f
Samuel Adolfo Cruz Alegria <scruzale@daint104.login.cscs.ch>
2024-05-22 10:20:43 +0200

    Minor changes for potential tests later.

--- src/shell_scripts/parallel_test.sh
+++ src/shell_scripts/parallel_test.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
 # Define the arrays for each parameter
-optimizers=("APTS")
+optimizers=("SGD")
 batch_sizes=(200)
 trials=5
-epochs=50
+epochs=10
 nodes=2
 datasets=("mnist")
 
 # Iterate over each combination of parameters
 for optimizer in "${optimizers[@]}"
 do
-    # If optimizer is SGD, set learning rate to 0.1. Else, 0.001
-    if [ "$optimizer" == "SGD" ]
-    then
-        learning_rate=0.1
-    else
-        learning_rate=0.001
-    fi
-
     for batch_size in "${batch_sizes[@]}"
     do   
         for dataset in "${datasets[@]}"
         do
+            # If optimizer is SGD, set learning rate to 0.1. Else, 0.001
+            if [ "$optimizer" == "SGD" ]
+            then
+                # If dataset is CIFAR-10, set learning rate to 0.01
+                if [ "$dataset" == "cifar10" ]
+                then
+                    learning_rate=0.01
+                else
+                    learning_rate=0.01
+                fi
+            else
+                if [ "$dataset" == "mnist" ]
+                then
+                    learning_rate=0.001
+                else
+                    learning_rate=0.001
+                fi
+            fi
+
             # Make job name depend on the parameters
             job_name="test_${optimizer}_${batch_size}_${learning_rate}_${dataset}"
             error_file="${job_name}.err"
