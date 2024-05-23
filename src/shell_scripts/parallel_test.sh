b51c70a73739f6b012f88f26d6ede00212c1596a
Samuel Adolfo Cruz Alegria <scruzale@daint106.login.cscs.ch>
2024-05-23 13:36:51 +0200

    Some changes to test. Co-author: Ken Trotti.

--- src/shell_scripts/parallel_test.sh
+++ src/shell_scripts/parallel_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Define the arrays for each parameter
-optimizers=("APTS")
+optimizers=("SGD" "Adam")
 batch_sizes=(200)
-trials=5
+trials=3
 epochs=50
-nodes=2
+nodes=2 
 datasets=("cifar10")
-
 # Iterate over each combination of parameters
 for optimizer in "${optimizers[@]}"
 do
@@ -44,4 +43,46 @@ do
     done
 done
 
-echo "All tests submitted to cluster."
+# echo "All SGD/Adam tests submitted to cluster."
+# optimizers=("APTS")
+# nodes=(2 4 8) 
+# # Iterate over each combination of parameters
+# for optimizer in "${optimizers[@]}"
+# do
+#     for batch_size in "${batch_sizes[@]}"
+#     do   
+#         for dataset in "${datasets[@]}"
+#         do
+#             for node in "${nodes[@]}"
+#             do
+#                 # If optimizer is SGD, set learning rate to 0.1. Else, 0.001
+#                 if [ "$optimizer" == "SGD" ]
+#                 then
+#                     # If dataset is CIFAR-10, set learning rate to 0.01
+#                     if [ "$dataset" == "cifar10" ]
+#                     then
+#                         learning_rate=0.01
+#                     else
+#                         learning_rate=0.01
+#                     fi
+#                 else
+#                     if [ "$dataset" == "mnist" ]
+#                     then
+#                         learning_rate=0.001
+#                     else
+#                         learning_rate=0.001
+#                     fi
+#                 fi
+#                 # Make job name depend on the parameters
+#                 job_name="test_${optimizer}_beta6_${dataset}_${batch_size}_${learning_rate}_${epochs}_${trials}_${node}"
+#                 error_file="${job_name}.err"
+#                 output_file="${job_name}.out"
+                
+#                 sbatch --nodes="$node" --job-name="$job_name" --output="$output_file" --error="$error_file" parallel_test.job "$optimizer" "$batch_size" "$learning_rate" "$trials" "$epochs" "$dataset"
+#             done
+#         done
+#     done
+# done
+
+
+
