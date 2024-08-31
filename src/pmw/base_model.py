Found at least one bug, which is that to is asynchronous for CUDA devices, so tensors must be cast to CUDA prior to a broadcast, not within the broadcast call itself.

src/pmw/base_model.py
@@ -10,8 +10,8 @@ class BaseModel(nn.Module):
         self.rank = dist.get_rank()
         self.world_size = dist.get_world_size()
         self.backend = dist.get_backend()
-        self.tensor_device = 'cuda'
-        self.default_device = 'cpu' # Default device to store scalars
+        self.tensor_device = torch.device('cuda')
+        self.default_device = torch.device('cpu') # Default device to store scalars
         
     def backend_device(self, tensor=torch.tensor([0])):
-        return 'cpu' if dist.get_backend() == 'gloo' else (str(tensor.device) if 'cuda' in str(tensor.device) else 'cuda')
+        return torch.device('cpu') if dist.get_backend() == 'gloo' else (tensor.device if 'cuda' in str(tensor.device) else torch.device('cuda'))

src/utils.py
@@ -84,14 +84,14 @@ def closure(inputs, targets, criterion, model, compute_grad=True, zero_grad=True
         if zero_grad:
             model.zero_grad()
         with torch.set_grad_enabled(compute_grad):
-            print(f"Num inputs: {len(inputs)}, inputs[0] shape: {inputs[0].shape}")
+            print(f"Rank {dist.get_rank()} num inputs: {len(inputs)}, inputs shape: {inputs.shape}")
             outputs = model(inputs, chunks_amount=data_chunks_amount)
+            print(f"Rank {dist.get_rank()} num outputs: {len(outputs)}, Num targets: {len(targets)}")
         losses = [0] * data_chunks_amount
         loss = torch.tensor(0.0).to(model.tensor_device)
         if model.rank in model.all_stage_ranks[-1]:
-            print(f"Num outputs: {len(outputs)}, Num targets: {len(targets)}")
             for i, out in enumerate(outputs):
-                print(f"Output shape: {out.shape}, Target shape: {targets[i].shape}")
+                print(f"Rank {dist.get_rank()} output shape: {out.shape}, Target shape: {targets[i].shape}")
                 losses[i] = criterion(out, targets[i].to(out.device))
             loss = sum(losses)/len(losses)
         # Average losses across replicas

tests/scaling_test.py
@@ -46,7 +46,7 @@ def main(args, rank=None, master_addr=None, master_port=None, world_size=None):
     # Make sure all ranks have the same number of GPUs
     utils.check_gpus_per_rank()
     rank = dist.get_rank() if dist.get_backend() == 'nccl' else rank
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # Set seed    
     seed = 123456789 * parsed_args.trial
@@ -93,7 +93,7 @@ def main(args, rank=None, master_addr=None, master_port=None, world_size=None):
     if parsed_args.dataset.lower() == "mnist":
         input_size = 784
         # random_input = torch.randn(10, 1, 784, device=device)
-        random_input = torch.randn(10, 1, input_size, device=device)
+        random_input = torch.randn(parsed_args.batch_size, 1, input_size, device=device)
     elif parsed_args.dataset.lower() == "cifar10":
         # TODO: Verify whether this works or not
         raise NotImplementedError("CIFAR-10 dataset is not implemented yet.")
@@ -143,7 +143,7 @@ def main(args, rank=None, master_addr=None, master_port=None, world_size=None):
             x = x.to(device)
             y = y.to(device)
 
-            print(f"Train loader x shape: {x.shape}, y shape: {y.shape}")
+            print(f"Rank {dist.get_rank()} train loader x shape: {x.shape}, y shape: {y.shape}")
 
             # Gather parallel model norm
             par_optimizer.zero_grad()